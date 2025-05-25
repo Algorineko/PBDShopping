398cad6c2e14d309d06b7ea385130af79288e43b
Ncu-uu <9109222099@email.ncu.edu.cn>
2025-05-25 21:36:06 +0800

    增加了商家的业务逻辑

--- backend/src/main/java/com/pbdcompany/entity/Logisticsinfo.java
+++ backend/src/main/java/com/pbdcompany/entity/Logisticsinfo.java
@@ -1,5 +1,6 @@
 package com.pbdcompany.entity;
 
+import com.pbdcompany.enums.DeliveryStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ public class Logisticsinfo {
     private int orderItemId;
     private String logisticsCompany;
     private String trackingNumber;
-    private String status;
+    private DeliveryStatus status;
 
 //    private LocalDateTime addedAt;
 //    private LocalDateTime updatedAt;
